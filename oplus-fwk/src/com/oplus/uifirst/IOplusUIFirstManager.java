78be23d2a172266e0052a60a932f872d9142cd9f
pjgowtham <pjgowtham@gmail.com>
2025-12-28 09:12:50 +0530

    fixup!^2 oplus-fwk: Add missing classes for oplus camera
    Change-Id: I7942bcd847ce70e23e5502782017399beaef55c5

--- oplus-fwk/src/com/oplus/uifirst/IOplusUIFirstManager.java
+++ oplus-fwk/src/com/oplus/uifirst/IOplusUIFirstManager.java
@@ -8,5 +8,5 @@ package com.oplus.uifirst;
 public interface IOplusUIFirstManager {
     default void setBinderThreadUxFlag(int pid, int flag) {}
 
-    default void setUxThreadValue(int pid, int tid, String value) {}
+    default void setUxThreadValue(int pid, int flag, String tag) {}
 }
